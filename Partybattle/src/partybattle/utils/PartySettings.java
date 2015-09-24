e011f1f36c24bdee617445493fc6c6293bd17a6e
Emil Ohlsson <kotte@kottland.net>
2015-09-24 13:27:23 +0200

    Started using GSON for reading JSON structures, and used that to read settings.

--- Partybattle/src/partybattle/utils/PartySettings.java
+++ Partybattle/src/partybattle/utils/PartySettings.java
@@ -1,8 +1,77 @@
 package partybattle.utils;
 
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.lang.reflect.Type;
+
 import javax.swing.ImageIcon;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonDeserializationContext;
+import com.google.gson.JsonDeserializer;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParseException;
+
 public class PartySettings {
+	private class Guest {
+		String name;
+		int col;
+		int row;
+	}
+	private class Boat {
+		String name;
+		Guest[] guests;
+	}
+	private class Settings {
+		int cols;
+		int rows;
+		
+		String backgroundImage;
+		String explosionImage;
+		String boatImage;
+		
+		Guest[] guestsOfHonor;
+		Boat[] boats;
+	}
+	
+	private class SettingsDeserializer implements JsonDeserializer<Settings> {
+
+		@Override
+		public Settings deserialize(JsonElement json, Type type, JsonDeserializationContext context)
+				throws JsonParseException {
+			final JsonObject thisObject = json.getAsJsonObject();
+			Settings settings = new Settings();
+			settings.cols = thisObject.get("cols").getAsInt();
+			settings.rows = thisObject.get("rows").getAsInt(); 
+			
+			settings.backgroundImage = thisObject.get("backgroundImage").getAsString();
+			settings.explosionImage = thisObject.get("explosionImage").getAsString();
+			settings.boatImage = thisObject.get("boatImage").getAsString();
+			
+			settings.guestsOfHonor = context.deserialize(thisObject.get("guestsOfHonor"), Guest[].class);
+			
+			JsonArray jsonBoats = thisObject.get("boats").getAsJsonArray(); 
+			
+			settings.boats = new Boat[jsonBoats.size()];
+			for (int i = 0; i < jsonBoats.size(); i++) {
+				JsonObject jsonBoat = jsonBoats.get(i).getAsJsonObject();
+				Boat boat = new Boat();
+				boat.name = jsonBoat.get("name").getAsString();
+				boat.guests = context.deserialize(jsonBoat.get("guests"), Guest[].class);
+				settings.boats[i] = boat;
+				
+			}
+			
+			return settings;
+		}
+		
+	}
+	
 	private int cols;
 	private int rows;
 	
@@ -18,64 +87,53 @@ public class PartySettings {
 		}
 	}
 	
-	public PartySettings(String settingsFilePath) {
-		/* TODO open the given file
-		 * 
-		 * Probably good if the file is of JSON format. e.g.,
-		 * 
-		 * {
-		 *    "cols": 7,
-		 *    "rows": 7,
-		 *    "backgroundImage": "/path/to/image"
-		 *    "boats": [
-		 *        [
-		 *            {
-		 *                "name": "Foo Barsson",
-		 *                "col": 5,
-		 *                "row": 7
-		 *            },
-		 *            {
-		 *                "name": "Bar Foosson",
-		 *                "col": 5,
-		 *                "row": 7
-		 *            }
-		 *        ]
-		 *    ],
-		 *    "guestOfHonors": [
-		 *        {
-		 *            "name": "Ufuk",
-		 *            "col": 1,
-		 *            "row": 1
-		 *        }
-		 *    ]
-		 *    }
-		 * }
-		 */
+	public PartySettings(String settingsFilePath) throws IOException {
+		GsonBuilder gsonBuilder = new GsonBuilder();
+		gsonBuilder.registerTypeAdapter(Settings.class, new SettingsDeserializer());
+		Gson gson = gsonBuilder.create();
 		
-		// TODO read settings to find out cols and rows
-		cols = 8;
-		rows = 8;
-		guests = new PartyGuest[cols][rows];
+		InputStream inputStream = new FileInputStream(settingsFilePath);
+		InputStreamReader reader = new InputStreamReader(inputStream);
+		Settings settings = gson.fromJson(reader, Settings.class);
 		
-		mapImagePath = "map.jpg";
-		explosionImagePath = "fire.png";
-		boatImagePath = "boat.png";
+		PartyLog.log("Reader cols = " + settings.cols + ", rows = " + settings.rows);
 		
-		PartyBoat boat;
+		PartyLog.log("The following guests are special");
+		for (Guest guest : settings.guestsOfHonor) {
+			PartyLog.log(guest.name + " at " + guest.col + ", " + guest.row);
+		}
 		
-		boat = new PartyBoat("MS Whatknot");
-		addGuestAt("John Dorian", boat, 3, 3);
-		addGuestAt("Christoffer Turk", boat, 3, 4);
+		PartyLog.log("The following boats exist");
+		for (Boat boat : settings.boats) {
+			PartyLog.log("boat: " + boat.name);
+			for (Guest guest : boat.guests) {
+				PartyLog.log(guest.name + " at " + guest.col + ", " + guest.row);
+			}
+		}
 		
-		boat = new PartyBoat("MS Herpaderp");
-		addGuestAt("Perry Cox", boat, 2, 2);
-		addGuestAt("Dr. Kelso", boat, 2, 3);
+		this.boatImagePath = settings.boatImage;
+		this.explosionImagePath = settings.explosionImage;
+		this.mapImagePath = settings.backgroundImage;
 		
-		addGuestAt("Ufuk Kirik", null, 1, 1);
+		this.cols = settings.cols;
+		this.rows = settings.rows;
+		
+		this.guests = new PartyGuest[this.cols][this.rows];
+		
+		for (Guest guest : settings.guestsOfHonor) {
+			addGuestAt(guest.name, null, guest.col, guest.row);
+		}
+		
+		for (Boat boat : settings.boats) {
+			PartyBoat tmpBoat = new PartyBoat(boat.name);
+			for (Guest guest : boat.guests) {
+				addGuestAt(guest.name, tmpBoat, guest.col, guest.row);	
+			}
+		}
 	}
 	
-	public PartySettings() {
-		this("/replace/me/with/good/path/later");
+	public PartySettings() throws IOException {
+		this("settings.json");
 	}
 
 	public int getRows() {

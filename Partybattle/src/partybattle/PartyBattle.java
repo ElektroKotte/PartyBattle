e151807017d12a3c09f9f31ce4087831d95cfeed
fickludd <johan.teleman@gmail.com>
2015-09-30 22:28:32 +0200

    Added loading of guestLIst and board size

--- Partybattle/src/partybattle/PartyBattle.java
+++ Partybattle/src/partybattle/PartyBattle.java
@@ -2,6 +2,10 @@ package partybattle;
 
 import java.util.LinkedList;
 import java.util.List;
+import java.io.File;
+import java.io.FileReader;
+import java.io.BufferedReader;
+import java.io.IOException;
 
 import javax.swing.SwingUtilities;
 
@@ -9,13 +13,15 @@ import partybattle.gui.*;
 
 public class PartyBattle implements Runnable {
 
-	final int COLS = 8;
-	final int ROWS = 8;
+	int COLS = 8;
+	int ROWS = 8;
 	
 	List<String> guests;
 	
-	public PartyBattle(List<String> guests) {
+	public PartyBattle(int n, List<String> guests) {
 		this.guests = guests;
+		COLS = n;
+		ROWS = n;
 	}
 	
 	public void run() {
@@ -24,7 +30,21 @@ public class PartyBattle implements Runnable {
 	}
 	
 	public static void main(String[] args) {
-		SwingUtilities.invokeLater(new PartyBattle(someGuests()));	
+		if (args.length < 2) {
+			System.out.println("missing n and guestlist. Good bye!");
+			System.exit(1);
+		}
+		
+		int n = Integer.parseInt(args[0]);
+		File guestList = new File(args[1]);
+			
+		try {
+			SwingUtilities.invokeLater(new PartyBattle(n, readGuests(guestList)));
+		} catch(IOException e) {
+			System.out.println("Failed reading guestList");
+			e.printStackTrace();
+			System.exit(1);
+		}
 	}
 	
 	public static List<String> someGuests() {
@@ -43,8 +63,15 @@ public class PartyBattle implements Runnable {
 		return l;
 	}
 
-	public static List<String> readGuests() {
+	public static List<String> readGuests(File f) throws IOException {
 		List<String> l = new LinkedList<String>();
+		BufferedReader r = new BufferedReader(new FileReader(f));
+		String line = r.readLine(); 
+		while (line != null) {
+			l.add(line);
+			line = r.readLine();
+		}
+		r.close();
 		// TODO: Here goes code that opens a file with one guest per line and reads it.
 		return l;
 	}
